6523f776d26b37028daaff892deefe4170280635
centum-world <128315197+centum-world@users.noreply.github.com>
2023-07-20 14:36:15 +0530

    Revert "navbar and hero updated"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,107 +1,109 @@
-import React, { useContext, useState } from "react";
-import "../css/Navbar.css";
-import { NavLink } from "react-router-dom";
-import { UserContext } from "../App";
-import UserRegistration from "./UserRegistration";
-import AdminLogin from "./AdminLogin";
-import UserLogin from "./UserLogin";
-import Button from "react-bootstrap/Button";
-import logo from "./../img/logo1.png";
+import React, { useContext, useState } from 'react'
+import '../css/Navbar.css'
+import { NavLink } from 'react-router-dom';
+import { UserContext } from '../App';
+import UserRegistration from './UserRegistration';
+import AdminLogin from './AdminLogin';
+import UserLogin from './UserLogin';
+import Button from 'react-bootstrap/Button';
+import logo from './../img/logo1.png';
+
 
 function Navbar() {
-  const { state, dispatch } = useContext(UserContext);
-  const login = localStorage.getItem("login");
+    const { state, dispatch } = useContext(UserContext);
+    const login = localStorage.getItem('login');
 
-  const [userShow, setUserShow] = useState(false);
-  const [adminShow, setAdminShow] = useState(false);
 
-  const openUserLoginFuction = () => setUserShow(true);
-  const pull_data = (data) => setUserShow(data);
+    const [userShow, setUserShow] = useState(false);
+    const [adminShow, setAdminShow] = useState(false);
 
-  const openAdminLoginFuction = () => setAdminShow((prev) =>  !prev);
-  const pull_addmin = (data) => setAdminShow(data);
+    const openUserLoginFuction = () => setUserShow(true);
+    const pull_data = (data) => setUserShow(data);
 
-  const RenderMenu = () => {
-    if (login) {
-      return (
-        <>
-          <li className="nav-item">
-            <NavLink
-              className="btn btn-outline-dark rounded-pill "
-              to="/logout"
-              aria-current="page"
-            >
-              Logout
-            </NavLink>
-          </li>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <li className="nav-item">
-            {/* <button className=" btn rounded btn-outline-primary rounded-pill" data-bs-toggle="modal" data-bs-target="#adminLogin" aria-current="page">Admin</button> */}
-            <Button
-              variant=" btn rounded btn-outline-primary rounded-pill"
-              onClick={openAdminLoginFuction}
-            >
-              Admin
-            </Button>
-          </li>
-          &nbsp;&nbsp;
-          {/* <li className="nav-item">
+
+
+    const openAdminLoginFuction = () => setAdminShow(true);
+    const pull_addmin = (data) => setAdminShow(data);
+
+    const RenderMenu = () => {
+        if (login) {
+            return (
+
+                <>
+
+                    <li className="nav-item">
+                        <NavLink className="btn btn-outline-dark rounded-pill " to="/logout" aria-current="page" >Logout</NavLink>
+                    </li>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <li className="nav-item">
+                        {/* <button className=" btn rounded btn-outline-primary rounded-pill" data-bs-toggle="modal" data-bs-target="#adminLogin" aria-current="page">Admin</button> */}
+                        <Button variant=" btn rounded btn-outline-primary rounded-pill" onClick={openAdminLoginFuction}>
+                            Admin
+                        </Button>
+                    </li>&nbsp;&nbsp;
+                    {/* <li className="nav-item">
                        
                         <Button variant=" btn rounded btn-outline-primary rounded-pill" onClick={openUserLoginFuction}>
                             User
                         </Button>
                        
                     </li> */}
-        </>
-      );
-    }
-  };
 
-  return (
-    <>
-      <nav className="navbar navbar-box navbar-expand-lg navbar-light bg-light">
-        {/* <div className="container-fluid"> */}
-        <div className="navbar-brand">
-          <div>
-            <h3>JETTRADE FX </h3>
-          </div>
-          <div>
-            <img src={logo} alt="" style={{ width: "100px", height: "35px" }} />
-          </div>
-          <div>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-          </div>
-        </div>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <RenderMenu />
-          </ul>
-        </div>
-        {/* </div> */}
-        {/* {userShow?
+
+
+                </>
+            )
+        }
+    }
+
+    return (
+        <>
+            <nav className="navbar navbar-box navbar-expand-lg navbar-light bg-light">
+                {/* <div className="container-fluid"> */}
+                <div className="navbar-brand">
+                    <div>
+                        <h3>JETTRADE FX </h3>
+                    </div>
+                    <div>
+                        <img src={logo} alt="" style={{ width: '100px', height: '35px' }} />
+                    </div>
+                    <div>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon" />
+                        </button>
+                    </div>
+                </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <RenderMenu />
+
+
+                    </ul>
+
+                </div>
+                {/* </div> */}
+                {/* {userShow?
                     <UserLogin func={pull_data}/>:''
 
                 } */}
 
-        {/* Admin Login */}
-        {adminShow ? <AdminLogin adminFunc={pull_addmin} /> : ""}
-      </nav>
-    </>
-  );
+
+                {/* Admin Login */}
+                {adminShow ?
+                    <AdminLogin adminFunc={pull_addmin} /> : ''
+                }
+
+            </nav>
+
+
+
+
+        </>
+    )
 }
 
-export default Navbar;
+export default Navbar
