15d6eaefe0cd6c5b8e672c0ea111931b71d33372
prashantpkc <prashantkumarpkc@gmail.com>
2023-07-22 14:49:05 +0530

    video uplaod design

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,3 +1,143 @@
+// import React, { useState, useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Axios from "axios";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { UserContext } from "../App";
+// import Button from "react-bootstrap/Button";
+// import Modal from "react-bootstrap/Modal";
+// import { Input, message } from "antd";
+// import { UserOutlined, UnlockOutlined } from "@ant-design/icons";
+// import baseUrl from "../baseUrl";
+
+// const apiurl = baseUrl.apiUrl;
+// function AdminLogin(props) {
+//   const { state, dispatch } = useContext(UserContext);
+//   let navigate = useNavigate();
+//   const [admin, setAdmin] = useState({
+//     admin_id: "",
+//     admin_password: "",
+//   });
+
+//   const [show, setShow] = useState(true);
+//   const handleClose = () => setShow((prev) =>  !prev);
+//   props.adminFunc(show);
+//   const handleInputs = (e) => {
+//     setAdmin({ ...admin, [e.target.name]: e.target.value });
+//     console.log(e.target.value);
+//   };
+
+//   const adminLogin = (e) => {
+//     e.preventDefault();
+//     Axios.post("http://localhost:4000/admin/login", {
+//       admin_id: admin.admin_id,
+//       password: admin.admin_password,
+//     })
+//       .then((response) => {
+//         dispatch({ type: "USER", payload: true });
+//         localStorage.setItem("login", true);
+//         localStorage.setItem("adminToken", response.data.token);
+//         localStorage.setItem("adminId", response.data.admin_id);
+//         admin.admin_id = "";
+//         admin.admin_password = "";
+//         navigate("/admindashboard");
+//       })
+//       .catch((error) => {
+//         console.log("Not login");
+//         if (error.response.status === 422) {
+//           toast.warning(
+//             "Please Fill all Details!",
+//             {
+//               autoClose: 2000,
+//               theme: "dark",
+//             },
+//             1000
+//           );
+//           // setTimeout(()=>{
+//           //     message.warning('Please fill all Details')
+//           // },1000);
+//           admin.admin_id = "";
+//           admin.admin_password = "";
+//         }
+//         if (error.response.status === 404) {
+//           toast.warning("Invalid Credential!", {
+//             autoClose: 2000,
+//             theme: "dark",
+//           });
+//         }
+//       });
+//     setShow(false);
+//   };
+
+//   return (
+//     <>
+//       <Modal show={show} onHide={handleClose}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>Admin Login</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <div className="form-content">
+//             <form onSubmit={adminLogin} autoComplete="off">
+//               <div className="row">
+//                 <div className="col">
+//                   <div className="form-group mb-3">
+//                     <label htmlFor="loginid"> LOGIN ID</label>
+//                     {/* <input type="text" name='admin_id' id='loginid' className='form-control' placeholder='Enter login ID'
+//                                             value={admin.admin_id}
+//                                             onChange={handleInputs}
+//                                         /> */}
+//                     <Input
+//                       placeholder="Enter login ID "
+//                       prefix={<UserOutlined />}
+//                       value={admin.admin_id}
+//                       name="admin_id"
+//                       allowClear
+//                       onChange={handleInputs}
+//                     />
+//                   </div>
+//                 </div>
+//               </div>
+
+//               <div className="row">
+//                 <div className="col">
+//                   <div className="form-group mb-3">
+//                     <label htmlFor="password">PASSWORD</label>
+//                     {/* <input type="password" name='admin_password' id='password' className='form-control' placeholder='Enter Password'
+//                                             value={admin.admin_password}
+//                                             onChange={handleInputs}
+//                                         /> */}
+//                     <Input.Password
+//                       placeholder="Enter your password"
+//                       type="password"
+//                       prefix={<UnlockOutlined />}
+//                       value={admin.admin_password}
+//                       name="admin_password"
+//                       onChange={handleInputs}
+//                     />
+//                   </div>
+//                 </div>
+//               </div>
+//               <Modal.Footer>
+//                 <Button variant="danger" type="submit">
+//                   Close
+//                 </Button>
+//                 <Button variant="success" type="submit">
+//                   Login
+//                 </Button>
+//               </Modal.Footer>
+//             </form>
+//           </div>
+//         </Modal.Body>
+//       </Modal>
+
+//       <ToastContainer />
+//     </>
+//   );
+// }
+
+// export default AdminLogin;
+
+
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
@@ -11,6 +151,7 @@ import { UserOutlined, UnlockOutlined } from "@ant-design/icons";
 import baseUrl from "../baseUrl";
 
 const apiurl = baseUrl.apiUrl;
+
 function AdminLogin(props) {
   const { state, dispatch } = useContext(UserContext);
   let navigate = useNavigate();
@@ -20,8 +161,9 @@ function AdminLogin(props) {
   });
 
   const [show, setShow] = useState(true);
-  const handleClose = () => setShow((prev) =>  !prev);
+  const handleClose = () => setShow((prev) => !prev);
   props.adminFunc(show);
+
   const handleInputs = (e) => {
     setAdmin({ ...admin, [e.target.name]: e.target.value });
     console.log(e.target.value);
@@ -38,8 +180,7 @@ function AdminLogin(props) {
         localStorage.setItem("login", true);
         localStorage.setItem("adminToken", response.data.token);
         localStorage.setItem("adminId", response.data.admin_id);
-        admin.admin_id = "";
-        admin.admin_password = "";
+        setAdmin({ admin_id: "", admin_password: "" }); // Use setAdmin to reset the state
         navigate("/admindashboard");
       })
       .catch((error) => {
@@ -53,11 +194,7 @@ function AdminLogin(props) {
             },
             1000
           );
-          // setTimeout(()=>{
-          //     message.warning('Please fill all Details')
-          // },1000);
-          admin.admin_id = "";
-          admin.admin_password = "";
+          setAdmin({ admin_id: "", admin_password: "" }); // Use setAdmin to reset the state
         }
         if (error.response.status === 404) {
           toast.warning("Invalid Credential!", {
@@ -82,10 +219,6 @@ function AdminLogin(props) {
                 <div className="col">
                   <div className="form-group mb-3">
                     <label htmlFor="loginid"> LOGIN ID</label>
-                    {/* <input type="text" name='admin_id' id='loginid' className='form-control' placeholder='Enter login ID'
-                                            value={admin.admin_id}
-                                            onChange={handleInputs}
-                                        /> */}
                     <Input
                       placeholder="Enter login ID "
                       prefix={<UserOutlined />}
@@ -102,10 +235,6 @@ function AdminLogin(props) {
                 <div className="col">
                   <div className="form-group mb-3">
                     <label htmlFor="password">PASSWORD</label>
-                    {/* <input type="password" name='admin_password' id='password' className='form-control' placeholder='Enter Password'
-                                            value={admin.admin_password}
-                                            onChange={handleInputs}
-                                        /> */}
                     <Input.Password
                       placeholder="Enter your password"
                       type="password"
@@ -118,7 +247,7 @@ function AdminLogin(props) {
                 </div>
               </div>
               <Modal.Footer>
-                <Button variant="danger" type="submit">
+                <Button variant="danger" onClick={handleClose}>
                   Close
                 </Button>
                 <Button variant="success" type="submit">
--- src/components/uservideo/Uservideo.js
+++ src/components/uservideo/Uservideo.js
@@ -3,87 +3,127 @@ import axios from "axios";
 
 const Uservideo = () => {
   const [videoTitle, setVideoTitle] = useState('');
-  const [upload, setupload] = useState('');
-
+  const [videoFile, setVideoFile] = useState(null);
+  const [thumbnailFile, setThumbnailFile] = useState(null);
 
   const handleTitleChange = (event) => {
     setVideoTitle(event.target.value);
   };
 
+  const handleVideoFileChange = (e) => {
+    setVideoFile(e.target.files[0]);
+  };
+
+  const handleThumbnailFileChange = (e) => {
+    setThumbnailFile(e.target.files[0]);
+  };
+
   const handleUpload = async (e) => {
     e.preventDefault();
-    
-    const formdata=new FormData();
-    formdata.append("title",videoTitle);
-    for(var i=0;i<upload.length;i++){
-      formdata.append("videoOne",upload[i]);
+
+    if (!videoTitle || !videoFile || !thumbnailFile) {
+      alert('Please fill all fields and select both video and thumbnail files');
+      return;
     }
+
+    const formData = new FormData();
+    formData.append("title", videoTitle);
+    formData.append("videoOne", videoFile);
+    formData.append("thumbnail", thumbnailFile);
+
     try {
-      await axios.post('/admin/createvideo', formdata, {
+      await axios.post('/admin/createvideo', formData, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('adminToken')}`,
+          'Content-Type': 'multipart/form-data', // Set the content type to 'multipart/form-data'
         },
       });
-      alert('Video uploaded successfully!');
+
+      alert('Video and Thumbnail uploaded successfully!');
+      // Optionally, you can reset the form after successful upload
+      setVideoTitle('');
+      setVideoFile(null);
+      setThumbnailFile(null);
     } catch (error) {
-      alert('Failed to upload video!');
+      alert('Failed to upload video and/or thumbnail!');
     }
   };
 
-
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        maxWidth: '400px',
-        height: '300px',
-        padding: '20px',
-        margin: '50px auto',
-        border: '1px solid #ccc',
-        borderRadius: '5px',
-        boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
-        boxSizing: 'border-box',
-        transition: 'box-shadow 0.3s ease-in-out',
-      }}
-    >
-      <h1 style={{ textAlign: 'center' }}>Video Upload</h1>
-      <div style={{ marginBottom: '10px' }}>
-        <label htmlFor="videoTitle" style={{ marginBottom: '5px' }}>
-          Video Title:
-        </label>
-        <input
-          type="text"
-          id="videoTitle"
-          value={videoTitle}
-          onChange={handleTitleChange}
-          style={{ width: '100%', padding: '5px' }}
-        />
-      </div>
-      <div style={{ marginBottom: '10px' }}>
-        {/* Add your video upload component here */}
-        <input type="file" style={{ width: '100%' }} defaultValue={upload} onChange={(e) => setupload(e.target.files)} />
-      </div>
-      <br />
-      <button
-        onClick={handleUpload}
+ 
+    return (
+      <div
         style={{
-          backgroundColor: '#4CAF50',
-          color: 'white',
-          padding: '10px',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
           width: '100%',
+          maxWidth: '400px',
+          height: '400px',
+          padding: '20px',
+          margin: '50px auto',
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+          boxSizing: 'border-box',
+          transition: 'box-shadow 0.3s ease-in-out',
         }}
       >
-        Upload Video
-      </button>
-    </div>
-  );
-};
-
-export default Uservideo;
+        <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>Video Upload</h1>
+        <div style={{ marginBottom: '20px', width: '100%' }}>
+          <label htmlFor="videoTitle" style={{ marginBottom: '5px', display: 'block' }}>
+            Video Title:
+          </label>
+          <input
+            type="text"
+            id="videoTitle"
+            value={videoTitle}
+            onChange={handleTitleChange}
+            style={{ width: '100%', padding: '5px' }}
+          />
+        </div>
+        <div style={{ marginBottom: '20px', width: '100%' }}>
+          <label htmlFor="videoFile" style={{ marginBottom: '5px', display: 'block' }}>
+            Video File:
+          </label>
+          <input
+            type="file"
+            id="videoFile"
+            onChange={handleVideoFileChange}
+            style={{ width: '100%' }}
+          />
+        </div>
+        <div style={{ marginBottom: '20px', width: '100%' }}>
+          <label htmlFor="thumbnailFile" style={{ marginBottom: '5px', display: 'block' }}>
+            Thumbnail:
+          </label>
+          <input
+            type="file"
+            id="thumbnailFile"
+            onChange={handleThumbnailFileChange}
+            style={{ width: '100%' }}
+          />
+        </div>
+        <br />
+        <button
+          onClick={handleUpload}
+          style={{
+            backgroundColor: '#3b03d8',
+            color: 'white',
+            padding: '10px',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            width: '100%',
+            maxWidth: '300px',
+            transition: 'background-color 0.2s ease',
+          }}
+        >
+          Upload Video
+        </button>
+      </div>
+    );
+  };
+  
+  export default Uservideo;
+  
