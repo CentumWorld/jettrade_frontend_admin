c5297422afe59180cba25d33fe1236c65b865450
centum-world <centumworldminingrig@gmail.com>
2023-08-29 17:13:40 +0530

    frenchise chat state chat

--- src/utils/StateAdminRoutes/StateLiveChat.js
+++ src/utils/StateAdminRoutes/StateLiveChat.js
@@ -7,22 +7,25 @@ import axios from 'axios';
 import baseUrl from '../../baseUrl';
 
 
+
 const apiurl = baseUrl.apiUrl
 
-const socket = io.connect('http://103.149.68.19:8081');
+// const socket = io.connect('http://103.149.68.19:8081');
+const socket = io.connect('http://localhost:4000');
 
 const StateLiveChat = () => {
-    const stateFname = localStorage.getItem('statefname');
-    const stateAdmin_id = localStorage.getItem('stateuserid');
+    const stateFname = localStorage.getItem('stateHandlerName');
+    const stateHandlerId = localStorage.getItem('stateHandlerId');
 
     const [statename, setStatename] = useState(stateFname);
-    const [room, setRoom] = useState(stateAdmin_id );
+    const [room, setRoom] = useState(stateHandlerId );
     const [showChat, setShowChat] = useState(false);
     const [type, setType] = useState('STATE');
 
     const joinRoom = () => {
         if (statename !== "" && room !== "") {
             socket.emit("join_room", room, type);
+            console.log(statename,room,type,'27')
             setShowChat(true);
         }
     }
@@ -34,7 +37,7 @@ const StateLiveChat = () => {
     const exitChatState = () => {
         let token = localStorage.getItem('stateHandlerToken')
         let data = {
-            stateHandlerId: localStorage.getItem('stateuserid')
+            stateHandlerId: localStorage.getItem('stateHandlerId')
         }
 
         let config = {
