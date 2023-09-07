3b3e9d92e24ecf6602bba729a8997d06107fc6fb
centum-world <centumworldminingrig@gmail.com>
2023-09-07 13:14:40 +0530

    subadmin block by admin

--- src/components/Admin/AdminSideBarPages/SubAdminDetails.js
+++ src/components/Admin/AdminSideBarPages/SubAdminDetails.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import "../css/NewRenewal.css";
-import { Menu, Tabs, Table, Input, Button, message } from "antd";
+import { Menu,Modal,Tabs, Table, Input, Button, message,Dropdown } from "antd";
 import axios from "axios";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { NavLink, useNavigate } from "react-router-dom";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import baseUrl from '../../../baseUrl';
 
 const apiurl = baseUrl.apiUrl
@@ -13,12 +14,18 @@ const { TabPane } = Tabs;
 const SubAdminDetails = () => {
     const navigate = useNavigate()
     const [subAdminData, setSubAdminData] = useState([]);
+    const [isBlocked, setIsBlock] = useState(true);
+    const [userStatus, setUserStatus] = useState(false);
+    const [myID, setMyID] = useState("");
+    const [userId, setUserID] = useState("");
     useEffect(() => {
         callApiFetchSubAdminData()
     }, [])
 
+    const token = localStorage.getItem('adminToken');
+
     const callApiFetchSubAdminData = () => {
-        const token = localStorage.getItem('adminToken');
+        
         const config = {
             headers: { "Authorization": `Bearer ${token}` }
         };
@@ -66,9 +73,108 @@ const SubAdminDetails = () => {
             dataIndex: 'gender',
             key: 'gender',
         },
-
+        {
+            title: "Block/Notblock",
+            dataIndex: "isBlocked",
+            render: (isBlocked) => {
+              const cellStyle = isBlocked ? { color: "red" } : { color: "green" };
+              return (
+                <span style={cellStyle}>
+                  {isBlocked ? "Blocked" : "Not Blocked "}
+                </span>
+              );
+            },
+          },
 
     ];
+    if (token) {
+        columns.push({
+          title: "Action",
+          dataIndex: "action",
+          render: (_, record) => (
+            <>
+              <Dropdown overlay={token ? menu :null} placement="bottomLeft" trigger={["click"]}>
+                <BsThreeDotsVertical
+                  size={24}
+                  onClick={() =>
+                    trigerAction(
+                      record._id,
+                      record.userid,
+                      record.status,
+                      record.isBlocked,
+                      
+                    )
+                  }
+                  style={{ cursor: "pointer" }}
+                />
+              </Dropdown>
+            </>
+          ),
+        })
+      }
+
+     
+
+      const trigerAction = (id, userid, status, block, subadmin) => {
+        setMyID(id, block);
+        setUserStatus(status);
+        setIsBlock(block);
+        setUserID(userid);
+        // setIsSubAdmin(subadmin)
+      };
+
+      const handleMenuClick = (e) => {
+        console.log(e.key);
+        // if (e.key === "edit") {
+        //    editModal();
+        //    fetchUserDetailsForEdit(myID);
+        //   console.log(myID);
+        // }
+        if (e.key === "block") {
+          blockUnblock(myID);
+        }
+      };
+
+
+       //block  or unblock
+
+  const blockUnblock = (id) => {
+    const actionText = isBlocked ? "Unblock" : "Block";
+    Modal.confirm({
+      title: `${actionText} User`,
+      content: `Are you sure you want to  ${actionText.toLowerCase()} this SubAdmin?`,
+      onOk() {
+        const token = localStorage.getItem("adminToken");
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const data = {
+          id: id,
+          block: !isBlocked,
+        };
+        axios
+          .post(`${apiurl}` + "/admin/block-subAdmin-by-Admin", data, config)
+          .then((res) => {
+            message.success(res.data.message);
+            callApiFetchSubAdminData()
+          })
+          .catch((err) => {
+            message.warning("Something went wrong!");
+          });
+      },
+      onCancel() {
+        console.log("Deletion cancelled");
+      },
+    });
+  };
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      {/* <Menu.Item key="edit">Edit</Menu.Item> */}
+      <Menu.Item key="block">{isBlocked ? "Unblock" : "Block"}</Menu.Item>
+    </Menu>
+  );
 
     return (
         <>
