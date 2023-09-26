50221cb1823e515156bd4225619e7523ebb7f1a6
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-09-26 13:47:39 +0530

    login button uncomment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -240,8 +240,8 @@ function Navbar() {
 
     const menu = (
       <Menu onClick={adminSubAdminModal}>
-        {/* <Menu.Item key="admin">Admin</Menu.Item>
-        <Menu.Item key="subadmin">Back Office</Menu.Item> */}
+        <Menu.Item key="admin">Admin</Menu.Item>
+        <Menu.Item key="subadmin">Back Office</Menu.Item>
         <Menu.Item key="sho">State Head Officer</Menu.Item>
         <Menu.Item key="franchise">Franchise</Menu.Item>
         <Menu.Item key="bussinessDev">BusinessDeveloper</Menu.Item>
